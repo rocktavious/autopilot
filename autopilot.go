b54288266c2d4b0442dd242327d8f8a156207794
Kyle <rocktavious@icloud.com>
2021-04-19 23:01:20 -0500

    Update autopilot.go

--- autopilot.go
+++ autopilot.go
@@ -2,10 +2,10 @@ package "autopilot"
 
 import (
 	"fmt"
-  "bytes"
+	"bytes"
 	"io/ioutil"
 	"net/http"
-  "net/http/httptest"
+	"net/http/httptest"
 	"path/filepath"
 	"runtime"
 	"reflect"
@@ -14,30 +14,30 @@ import (
 
 /*
 var (
-  Client *MyClient
+	Client *MyClient
 )
 
 func TestMain(m *testing.M) {
 	flag.Parse()
-  teardown := autopilot.Setup()
-  defer teardown()
-  Client := NewClient(autopilot.Server.URL)
+	teardown := autopilot.Setup()
+	defer teardown()
+	Client := NewClient(autopilot.Server.URL)
 	os.Exit(m.Run())
 }
 
 func TestExample(t *testing.T) {
-  // Arrange
+	// Arrange
 	autopilot.Mux.HandleFunc("/orgs/octokit/repos", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
 		fmt.Fprint(w, autopilot.Fixture("repos/octokit.json"))
 	})
-  // Act
+	// Act
 	body, err := Client.DoStuff()
-  a := Client.GetThing("Four")
-  // Assert
-  autopilot.Assert(t, a >= 4, "value must be greater than or equal to four")
-  autopilot.Ok(t, err)
+	a := Client.GetThing("Four")
+	// Assert
+	autopilot.Assert(t, a >= 4, "value must be greater than or equal to four")
+	autopilot.Ok(t, err)
 	autopilot.Equals(t, []byte("OK"), body)
 }
 */
@@ -69,18 +69,17 @@ func NewHttpClient(fn RoundTripFunc) *http.Client {
 	}
 }
 /*
-	client := autopilot.NewHttpClient(func(req *http.Request) *http.Response {
-		// Test request parameters
-		autopilot.Equals(t, req.URL.String(), "http://example.com/some/path")
-		return &http.Response{
-			StatusCode: 200,
-			Body: ioutil.NopCloser(bytes.NewBufferString(`OK`)),
- 			// Must be set to non-nil value or it panics
-			Header: make(http.Header),
-		}
-	})
-
-	api := API{client, "http://example.com"}
+client := autopilot.NewHttpClient(func(req *http.Request) *http.Response {
+	// Test request parameters
+	autopilot.Equals(t, req.URL.String(), "http://example.com/some/path")
+	return &http.Response{
+		StatusCode: 200,
+		Body: ioutil.NopCloser(bytes.NewBufferString(`OK`)),
+		// Must be set to non-nil value or it panics
+		Header: make(http.Header),
+	}
+})
+api := API{client, "http://example.com"}
 */
 
 // Load testdata/fixtures/<path> and return as string
@@ -95,26 +94,23 @@ func Fixture(path string) string {
 // Assert fails the test if the condition is false.
 func Assert(tb testing.TB, condition bool, msg string, v ...interface{}) {
 	if !condition {
-		_, file, line, _ := runtime.Caller(1)
-		fmt.Printf("\033[31m%s:%d: "+msg+"\033[39m\n\n", append([]interface{}{filepath.Base(file), line}, v...)...)
-		tb.FailNow()
+		tb.Helper()
+		tb.Fatalf(msg, v...)
 	}
 }
 
 // Ok fails the test if an err is not nil.
 func Ok(tb testing.TB, err error) {
 	if err != nil {
-		_, file, line, _ := runtime.Caller(1)
-		fmt.Printf("\033[31m%s:%d: unexpected error: %s\033[39m\n\n", filepath.Base(file), line, err.Error())
-		tb.FailNow()
+		tb.Helper()
+		tb.Fatalf("unexpected error: %s", err.Error())
 	}
 }
 
 // Equals fails the test if exp is not equal to act.
 func Equals(tb testing.TB, exp, act interface{}) {
 	if !reflect.DeepEqual(exp, act) {
-		_, file, line, _ := runtime.Caller(1)
-		fmt.Printf("\033[31m%s:%d:\n\n\texp: %#v\n\n\tgot: %#v\033[39m\n\n", filepath.Base(file), line, exp, act)
-		tb.FailNow()
+		tb.Helper()
+		tb.Fatalf("exp: %#v\n\n\tgot: %#v", exp, act)
 	}
 }
